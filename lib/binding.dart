264a5b93bb22be07b0c23e3574af08e0f6151df0
MouhssineGemaoui <117443378+MouhssineGemaoui@users.noreply.github.com>
2024-06-05 13:26:37 +0200

    Delete lib/binding.dart

--- lib/binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:monami/students/auth/controller/signup_controller.dart';
-
-class Mybinding extends Bindings {
-  @override
-  void dependencies() {
-     Get.lazyPut(() => SignUpcontrollerImp(),fenix: true );
-  }
-}
